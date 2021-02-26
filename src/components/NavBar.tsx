563344a6ea8e73909232f7d68a2001135b7d9161
Galen Guyer <galen@galenguyer.com>
2021-02-26 15:31:09 -0500

    Add unstyled NavBar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,84 @@
+import {
+  Collapse,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Nav,
+  NavItem,
+  NavLink,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+const NavBar = () => {
+  const [navbarIsOpen, setNavbarIsOpen] = useState(false);
+  const [dropdownIsOpen, setDropdownIsOpen] = useState(false);
+
+  const toggleNavbarIsOpen = () => setNavbarIsOpen(!navbarIsOpen);
+  const toggleDropdownIsOpen = () => setDropdownIsOpen(!dropdownIsOpen);
+
+  return (
+    <Navbar light expand="sm">
+      <NavbarBrand tag={Link} to="/">
+        <span>SSE @ RIT</span>
+      </NavbarBrand>
+      <NavbarToggler onClick={toggleNavbarIsOpen} />
+      <Collapse isOpen={navbarIsOpen} navbar>
+        <Nav className="mr-auto" navbar>
+          <NavItem>
+            <NavLink tag={Link} to="/events">
+              Events
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={Link} to="/projects">
+              Projects
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={Link} to="/go">
+              Go Links
+            </NavLink>
+          </NavItem>
+          <Dropdown nav isOpen={dropdownIsOpen} toggle={toggleDropdownIsOpen}>
+            <DropdownToggle nav caret>
+              About
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem>
+                <NavLink tag={Link} to="/about">
+                  About the SSE
+                </NavLink>
+              </DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>
+                <NavLink tag={Link} to="/officers">
+                  Officers
+                </NavLink>
+              </DropdownItem>
+              <DropdownItem>
+                <NavLink tag={Link} to="/governing-docs/constitution">
+                  SSE Constitution
+                </NavLink>
+              </DropdownItem>
+              <DropdownItem>
+                <NavLink
+                  tag={Link}
+                  to="/governing-docs/primary-officers-policy"
+                >
+                  SSE Primary Officers' Policy
+                </NavLink>
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
+};
+
+export default NavBar;
